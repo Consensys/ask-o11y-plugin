0f9b584ad65b44d3f36c8d8704a0b5736880e3d2
Alexandre Chappaz <alexandre.chappaz@consensys.net>
2026-02-02 18:12:14 +0100

    fix: version (#28)

--- src/components/Chat/components/WelcomeMessage/WelcomeMessage.test.tsx
+++ src/components/Chat/components/WelcomeMessage/WelcomeMessage.test.tsx
@@ -36,7 +36,7 @@ describe('WelcomeMessage', () => {
 
   it('should render the version number', () => {
     render(<WelcomeMessage />);
-    expect(screen.getByText('v0.2.2')).toBeInTheDocument();
+    expect(screen.getByText('v0.2.3')).toBeInTheDocument();
   });
 
   it('should render the description', () => {
